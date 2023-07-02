2472e66db4c38c077ec62aaab919b2a0399c955b
Maylen <maylendeulofeu@gmail.com>
2023-07-01 20:56:14 -0400

    Ask question and verify if correct

--- quiz-game.py
+++ quiz-game.py
@@ -4,3 +4,11 @@ playing = input("Do you want to play? ")
 
 if playing != "yes":
     quit()
+
+print("Okay! Let's play :) ")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else: 
+    print("Incorrect!")
