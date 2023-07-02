4ff0ec9d9fefbbed978d504f92a9c09dd1cc10a2
Maylen <maylendeulofeu@gmail.com>
2023-07-01 21:11:47 -0400

    Add more easy questions and additional info for incorrect answers

--- quiz-game.py
+++ quiz-game.py
@@ -7,8 +7,32 @@ if playing != "yes":
 
 print("Okay! Let's play :) ")
 
-answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+answer = input("What does the 'elif' keyword stand for? ")
+if answer == "else if":
     print("Correct!")
 else: 
-    print("Incorrect!")
+    print("Incorrect! Correct answer: else if")
+
+answer = input("What is the popular snake-inspired programming language? ")
+if answer == "python":
+    print("Correct!")
+else: 
+    print("Incorrect! Correct answer: python")
+
+answer = input("HTML stands for? ")
+if answer == "hypertext markup language":
+    print("Correct!")
+else: 
+    print("Incorrect! Correct answer: hypertext markup language")
+
+answer = input("What do you call a function that calls itself? ")
+if answer == "recursive function":
+    print("Correct!")
+else: 
+    print("Incorrect! Correct answer: recursive function")
+
+answer = input("What does the 'DOM' stand for in JavaScript? ")
+if answer == "document object model":
+    print("Correct!")
+else: 
+    print("Incorrect! Correct answer: document object model")
